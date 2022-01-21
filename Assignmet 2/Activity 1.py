c7d69f161c6cd4bfb2b2d0baf336a2d0b718a643
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-01-21 14:35:10 -0600

    Add files via upload

---
+++ Assignmet 2/Activity 1.py
@@ -0,0 +1,19 @@
+<?xml version="1.0"?>
+<flowgorithm fileversion="2.11">
+    <attributes>
+        <attribute name="name" value=""/>
+        <attribute name="authors" value="narde"/>
+        <attribute name="about" value=""/>
+        <attribute name="saved" value="2022-01-21 02:17:12 PM"/>
+        <attribute name="created" value="bmFyZGU7REVTS1RPUC1KSlJEN0ZNOzIwMjItMDEtMjE7MTE6NTQ6MDggQU07Mjg2Mw=="/>
+        <attribute name="edited" value="bmFyZGU7REVTS1RPUC1KSlJEN0ZNOzIwMjItMDEtMjE7MDI6MTc6MTIgUE07MTA7MzAyOA=="/>
+    </attributes>
+    <function name="Main" type="None" variable="">
+        <parameters/>
+        <body>
+            <declare name="age" type="String" array="False" size=""/>
+            <assign variable="age" expression="&quot;27 years old &quot;"/>
+            <output expression="&quot; I am &quot; &amp; age" newline="True"/>
+        </body>
+    </function>
+</flowgorithm>
