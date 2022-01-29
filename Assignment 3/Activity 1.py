8158cf05e4261802b3e67eac41e41cacfb3569b9
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-01-29 10:49:08 -0600

    Add files via upload

--- Assignment 3/Activity 1.py
+++ Assignment 3/Activity 1.py
@@ -4,10 +4,9 @@ print(" How many weekly hours did you work ?")
 hours = int(input())
 print(" What is your rate foy pay ?")
 rate = int(input())
-daily = hours * rate
-weekly = daily * 7
-monthly = daily * 30
+weekly = hours * rate
 yearly = weekly * 52
+monthly = float(yearly) / 12
 print("Salary in Weekly: " + str(weekly))
 print("Salary in Monthly: " + str(monthly))
 print("Salary in Yearly: " + str(yearly))
--- Assignment 3/Activity 5.py
+++ Assignment 3/Activity 5.py
@@ -1,8 +1,8 @@
 # Create a program that calculates the area of a room to determine the amount of floor covering required. The room is rectangular with the dimensions measured in feet with decimal fractions. The output needs to be in square yards. There are 3 linear feet (9 square feet) to a yard.
 print(" what is the lenght of rectangular in feet ")
-l = int(input())
+l = float(input())
 print(" What is the width of rectangular in feet ")
-w = int(input())
+w = float(input())
 area = l * w
-yeard = float(area) / 9
-print(str(yeard) + "  Square yeard")
+yeard = area / 9
+print(str(yard) + "  Square yard")
