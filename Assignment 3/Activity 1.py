b6a3d0424190360a96f3e894fc3ac76a0c266bd2
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-01-29 23:43:00 -0600

    Add files via upload

--- Assignment 3/Activity 1.py
+++ Assignment 3/Activity 1.py
@@ -3,10 +3,11 @@
 print(" How many weekly hours did you work ?")
 hours = int(input())
 print(" What is your rate foy pay ?")
-rate = int(input())
-weekly = hours * rate
+rate = float(input())
+daily = hours * rate
+weekly = daily * 7
 yearly = weekly * 52
-monthly = float(yearly) / 12
+monthly = daily * 30
 print("Salary in Weekly: " + str(weekly))
 print("Salary in Monthly: " + str(monthly))
 print("Salary in Yearly: " + str(yearly))
