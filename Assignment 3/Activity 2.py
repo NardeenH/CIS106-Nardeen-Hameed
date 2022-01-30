da489f862edc9b7e7445ed5128e36c069d7a8a2f
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-01-30 00:19:59 -0600

    Add files via upload

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -1,10 +1,13 @@
+# Create a program that asks the user how old they are in years, and then calculate and display their approximate age in months, days, hours, and seconds. For example, a person 1-year-old is 12 months old, 365 days old, etc.
+# 
+# I enjoy using flowchart.
 print(" How old are you ?")
 age = float(input())
 month = age * 12
 days = age * 12 * 365
 hours = (age * 12 * 365 + age / 4) * 24
-second = (age * 12 * 365 + age / 4) * 24 * 60 * 60
+seconds = (age * 12 * 365 + age / 4) * 24 * 60 * 60
 print(" age in month= " + str(month))
 print(" age in days= " + str(days))
 print(" age in hours = " + str(hours))
-print(" age in second = " + str(second))
+print(" age in seconds = " + str(seconds))
