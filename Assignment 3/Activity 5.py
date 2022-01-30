d35242b6481ba3e545cc9386dcdbc4240b8f1d66
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-01-29 23:00:02 -0600

    Add files via upload

--- Assignment 3/Activity 5.py
+++ Assignment 3/Activity 5.py
@@ -1,8 +1,8 @@
 # Create a program that calculates the area of a room to determine the amount of floor covering required. The room is rectangular with the dimensions measured in feet with decimal fractions. The output needs to be in square yards. There are 3 linear feet (9 square feet) to a yard.
-print(" what is the lenght of rectangular in feet ")
-l = float(input())
-print(" What is the width of rectangular in feet ")
-w = float(input())
-area = l * w
-yeard = area / 9
+print(" what is the lenght of rectangular in feet ?")
+lenght = float(input())
+print(" What is the width of rectangular in feet ?")
+width = float(input())
+area = lenght * width
+yard = area / 9
 print(str(yard) + "  Square yard")
