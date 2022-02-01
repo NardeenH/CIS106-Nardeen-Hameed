876073c2674afca042a15ba43e4b02eb87d36d79
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-01 12:46:35 -0600

    Add files via upload

---
+++ Assignment 4/Activity 1.py
@@ -0,0 +1,11 @@
+# Create a program to prompt the user for hours and rate per hour and then calculate and display their weekly, monthly, and annual gross pay (hours * rate). Base monthly and annual calculations on 12 months per year and 52 weeks per year.
+print(" How many weekly hours did you work ?")
+hours = float(input())
+print(" What is your rate foy pay ?")
+rate = float(input())
+weekly = rate * hours
+monthly = weekly * 30
+yearly = weekly * 365
+print("Salary in Weekly: " + str(weekly))
+print("Salary in Monthly: " + str(monthly))
+print("Salary in Yearly: " + str(yearly))
