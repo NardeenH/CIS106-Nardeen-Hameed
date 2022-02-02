496feffb09fb190f9ee17a6246063115f3683b76
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-02 09:00:01 -0600

    Add files via upload

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -1,12 +1,12 @@
 # Create a program that asks the user for a distance in miles, and then calculate and display the distance in yards, feet, and inches, or ask the user for a distance in miles, and then calculate and display the distance in kilometers, meters, and centimeters.
-print(" What is the Distance ? ")
-distance = int(input())
-distanceFeet = distance * 5280
-distanceYard = distance * 1760
-distanceInch = distance * 63360
-distanceKilometer = distance * 1.6069344
-distanceMeter = distance * 1609.34
-distanceCentimeter = distance * 160934
+print(" What is the Distance in Miles ? ")
+mile = int(input())
+distanceFeet = mile * 5280
+distanceYard = mile * 1760
+distanceInch = mile * 63360
+distanceKilometer = mile * 1.6069344
+distanceMeter = mile * 1609.34
+distanceCentimeter = mile * 160934
 print(" The distance in Feet is = " + str(distanceFeet))
 print(" The distance in Yard is = " + str(distanceYard))
 print(" The distance in Inch is = " + str(distanceInch))
