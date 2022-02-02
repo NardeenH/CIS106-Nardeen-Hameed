8784fd27aad001a39d67311d93e5e21834a35986
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-01 18:33:33 -0600

    Add files via upload

--- Assignment 4/Activity 1.py
+++ Assignment 4/Activity 1.py
@@ -1,12 +1,14 @@
-# Create a program to prompt the user for hours and rate per hour and then calculate and display their weekly, monthly, and annual gross pay (hours * rate). Base monthly and annual calculations on 12 months per year and 52 weeks per year.
-print(" How many weekly hours did you work ?")
-hours = int(input())
-print(" What is your rate foy pay ?")
-rate = int(input())
-salary = rate * hours
+# It's first time i will type in IDE. Let try to Create programe.
+#Create a program to prompt the user for hours and rate per hour and then calculate and display their weekly, monthly, and annual gross pay (hours * rate). Base monthly and annual calculations on 12 months per year and 52 weeks per year.
+print(" How many weekly hours you work ")
+Hours = float(input())
+print (" what is your rare for pay " )
+Rate = float( input())
+salary = Rate * Hours
 weekly = salary * 7
-monthly = salary * 30 * 12
+monthly = salary *30 * 12
 yearly = salary * 52 * 7
-print("Salary in Weekly: " + str(weekly))
-print("Salary in Monthly: " + str(monthly))
-print("Salary in Yearly: " + str(yearly))
+print ( " salary in weekly: " + str(weekly))
+print ( " salary in monthly: " + str(monthly))
+print ( " salary in yearly: " + str(yearly))
+
