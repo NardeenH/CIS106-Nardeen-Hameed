f1bc76e32e2c129dbb6767b636cbb6555a0e7384
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-02 09:05:35 -0600

    Add files via upload

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -3,13 +3,13 @@ print(" What is the Distance in Miles ? ")
 mile = int(input())
 distanceFeet = mile * 5280
 distanceYard = mile * 1760
-distanceInch = mile * 63360
+distanceInches = mile * 63360
 distanceKilometer = mile * 1.6069344
 distanceMeter = mile * 1609.34
 distanceCentimeter = mile * 160934
 print(" The distance in Feet is = " + str(distanceFeet))
 print(" The distance in Yard is = " + str(distanceYard))
-print(" The distance in Inch is = " + str(distanceInch))
+print(" The distance in Inches is = " + str(distanceInches))
 print(" The distance in kilometer is = " + str(distanceKilometer))
 print(" The distance in Meter is = " + str(distanceMeter))
 print(" The distance in centimeter is = " + str(distanceCentimeter))
