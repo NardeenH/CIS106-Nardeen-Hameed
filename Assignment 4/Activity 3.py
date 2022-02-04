31492deacea684847e471984aa05af26f545058e
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-03 19:32:24 -0600

    Add files via upload

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -6,7 +6,7 @@ DISTANCE_IN_FEET = miles * 5280
 DISTANCE_IN_YARD = miles * 1760
 DISTANCE_IN_INCHES = miles * 63360
 
-print("The distance is feet is = " + str(DISTANCE_IN_FEET))
-print("The distance is yard is = " + str(DISTANCE_IN_FEET))
-print("The distance is inches is = " + str(DISTANCE_IN_FEET))
+print("The distance in feet is = " + str(DISTANCE_IN_FEET))
+print("The distance in yard is = " + str(DISTANCE_IN_YARD))
+print("The distance in inches is = " + str(DISTANCE_IN_INCHES))
 
