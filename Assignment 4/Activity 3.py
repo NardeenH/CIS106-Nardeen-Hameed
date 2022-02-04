5d66b7e104c9a1a26f8fe10291c7d6172efc9b7c
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-03 18:31:23 -0600

    Add files via upload

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -1,18 +1,12 @@
-# Calculate the mileage in inches, yards, feet, kilometres, meters, and centimeters
+# Distance Converter
 
-mile = int("What is the distance in mile")
+miles = int(input("What is the distance in miles?"))
 
-d_Feet = mile * 5280
-d_Yard = mile * 1760
-d_Inches = mile * 63360
-d_kilometer = mile * 1.6069344
-d_Meter = mile * 1609.34
-d_centimeter = mile * 160934
+d_Feet = miles * 5280
+d_Yard = miles * 1760
+d_Inches = miles * 63360
 
 print("The distance is feet is = " + str(d_Feet))
 print("The distance is yard is = " + str(d_Yard))
 print("The distance is inches is = " + str(d_Inches))
-print("The distance is kilometer is = " + str(d_Kilometer))
-print("The distance is meter is = " + str(d_Feet))
-print("The distance is centimeter is = " + str(d_Centimeter))
 
