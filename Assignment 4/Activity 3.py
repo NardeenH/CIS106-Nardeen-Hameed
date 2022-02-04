607d073681667841c0b87a7f00001fa5e9cf3b87
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-03 19:35:29 -0600

    Add files via upload

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -2,11 +2,12 @@
 
 miles = int(input("What is the distance in miles?"))
 
-DISTANCE_IN_FEET = miles * 5280
-DISTANCE_IN_YARD = miles * 1760
-DISTANCE_IN_INCHES = miles * 63360
+DISTANCE_FEET = miles * 5280
+DISTANCE_YARD = miles * 1760
+DISTANCE_INCHES = miles * 63360
 
-print("The distance in feet is = " + str(DISTANCE_IN_FEET))
-print("The distance in yard is = " + str(DISTANCE_IN_YARD))
-print("The distance in inches is = " + str(DISTANCE_IN_INCHES))
+print("The distance in feet is = " + str(DISTANCE_FEET))
+print("The distance in yard is = " + str(DISTANCE_YARD))
+print("The distance in inches is = " + str(DISTANCE_INCHES))
+
 
