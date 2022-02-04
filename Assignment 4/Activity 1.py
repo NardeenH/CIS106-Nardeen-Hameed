59fce415284e695a33271a46c32740e1ffb0d2a6
Dave Braunschweig <dbraunsc@harpercollege.edu>
2022-02-04 09:00:35 -0600

    Assignment 4 corrections

--- Assignment 4/Activity 1.py
+++ Assignment 4/Activity 1.py
@@ -1,11 +1,14 @@
 # reading hours and rates from user 
 
+WEEKS_PER_MONTH = 4
+WEEKS_PER_YEAR = 52
+
 hours = float(input("How many weekly hours do you work?"))
 rate = float(input("What is the rate of pay?"))
 
 weekly = rate * hours 
-monthly = weekly * 4
-yearly = weekly * 52
+monthly = weekly * WEEKS_PER_MONTH
+yearly = weekly * WEEKS_PER_YEAR
 
 print("weekly = ", weekly)
 print("monthly = ", monthly)
