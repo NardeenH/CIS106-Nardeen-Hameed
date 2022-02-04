cd1f3e8ffc057b2ea21386a25a07d29e6482125e
Dave Braunschweig <dbraunsc@harpercollege.edu>
2022-02-04 08:59:21 -0600

    Assignment 4 corrections

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -1,11 +1,15 @@
 # Distance Converter
 
+YARDS_PER_MILE = 1760
+FEET_PER_MILE = 5280
+INCHES_PER_MILE = FEET_PER_MILE * 12
+
 miles = int(input("What is the distance in miles?"))
 
-DISTANCE_FEET = miles * 5280
-DISTANCE_YARD = miles * 1760
-DISTANCE_INCHES = miles * 63360
+feet = miles * FEET_PER_MILE
+yards = miles * YARDS_PER_MILE
+inches = miles * INCHES_PER_MILE
 
-print("The distance in feet is = " + str(DISTANCE_FEET))
-print("The distance in yard is = " + str(DISTANCE_YARD))
-print("The distance in inches is = " + str(DISTANCE_INCHES))
+print("The distance in feet is = " + str(feet))
+print("The distance in yard is = " + str(yards))
+print("The distance in inches is = " + str(inches))
