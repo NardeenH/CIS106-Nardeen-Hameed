8fa667d88a6e01c831c66e91867b7368f946accf
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-11 11:18:06 -0600

    Add files via upload

---
+++ Assignment 6/Activity 3.py
@@ -0,0 +1,34 @@
+# Distance Converter
+
+def get_mile():
+    print("Enter distance per mile")
+    miles = int(input())
+    return miles
+
+def calculate_mile_in_yard(miles):
+    yard = miles *1760
+    return yard
+
+def calculate_mile_in_feet(miles):
+    feet = miles*5280
+    return feet
+    
+    
+def calculate_mile_in_inches(miles):
+    inches = miles*12
+    return inches
+
+def desplay_result(yard, feet, inches):
+    print("The mile in yard is =" + str(yard))
+    print("The mile in feet is =" + str(feet))
+    print("The mile in inches is =" + str(inches))
+    
+    
+def main():
+    miles = get_mile()
+    yard = calculate_mile_in_yard(miles)
+    feet = calculate_mile_in_feet(miles)
+    inches = calculate_mile_in_inches(miles)
+    desplay_result(yard, feet, inches)
+
+main()
