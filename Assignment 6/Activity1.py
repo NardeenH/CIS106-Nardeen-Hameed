c41ded167c56d53ecaa623e16e1ae4d18435b7e0
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-10 22:02:47 -0600

    Add files via upload

---
+++ Assignment 6/Activity1.py
@@ -0,0 +1,38 @@
+# Prompt user for hours per week and rate per hour.
+
+def get_hourly():
+    print("How many hours per week?")
+    hourly = int(input())
+    return hourly
+
+def get_rate():
+    print("what's the rate per week?")
+    rate = int(input())
+    return rate
+
+def calculateWeekly(hourly, rate):
+    weekly = rate * hourly
+    return weekly
+
+def  calculateMonthly(weekly):
+    monthly = weekly * 4
+    return monthly
+
+def calculateYearly(weekly):
+    yearly = weekly * 52
+    return yearly
+
+def displayRuselt(weekly, monthly, yearly):
+    print("Salary in weekly is= " + str(weekly))
+    print("Salary in monthly is =" + str(monthly))
+    print("Salary in yearly is =" + str(yearly))
+    
+ # Main
+hourly = get_hourly()
+rate = get_rate()
+weekly = calculateWeekly(hourly, rate)
+monthly = calculateMonthly(weekly)
+yearly = calculateYearly(weekly)
+displayRuselt(weekly, monthly, yearly)
+
+
