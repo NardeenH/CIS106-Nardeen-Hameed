e5d33d327c0e1bca100039b8b0d3be4c4b39513f
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-22 20:37:02 -0600

    Add files via upload

--- Assignment 7/Activity 1.py
+++ Assignment 7/Activity 1.py
@@ -1,22 +1,30 @@
+def calPayment(hours, rate):
+    pay = hours * rate
+    print("The Payment for Regular Hours is" + str(pay))
+
 def getHours():
-    print("Enter hours")
-    hours = int(input())
+    print("Enter Hours:")
+    hours = float(input())
     
     return hours
 
 def getRate():
-    print("Enter rate per hours")
+    print("Enter Rate per Hour:")
     rate = float(input())
     
     return rate
 
+def overTimePayment(hours, rate):
+    pay = 40 * rate + (hours - 40) * 1.5 * rate
+    print("The payment for Overtime Hours is" + str(pay))
+
 # Main
-# Create a program to prompt the user for hours and rate per hour to compute gross pay (hours * rate). Include a calculation to give 1.5 times the hourly rate for any overtime (hours worked above 40 hours).[3] For example, 50 hours worked at $10 per hour with overtime is $550.
 hours = getHours()
 rate = getRate()
 if hours <= 40:
-    pay = hours * rate
+    calPayment(hours, rate)
 else:
     if hours > 40:
-        pay = 40 * rate + (hours - 40) * 1.5 * rate
-print(" GrossPay is " + str(pay))
+        overTimePayment(hours, rate)
+    else:
+        print("Put the right Data")
