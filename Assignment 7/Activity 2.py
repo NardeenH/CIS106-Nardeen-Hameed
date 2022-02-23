f31894d115e6ff0a4021a8cf69e80614844520b4
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-22 21:04:56 -0600

    Add files via upload

--- Assignment 7/Activity 2.py
+++ Assignment 7/Activity 2.py
@@ -1,3 +1,6 @@
+def dayResult(day):
+    print("Your Age in Days is:" + str(day))
+
 def getAge():
     print("How old you are in years?")
     userAge = int(input())
@@ -5,34 +8,43 @@ def getAge():
     return userAge
 
 def getAnswer():
-    print("If you Would like to know your age in months print M, in Day D  in hours H and in second S")
+    print("If you Would like to know your age print:M for months: D for days:H for hours: S for seconds:")
     userAnswer = input()
     
     return userAnswer
 
+def hoursResult(hour):
+    print("You Age in Hours is:" + str(hour))
+
+def monthResult(month):
+    print(" your age in months is:" + str(month))
+
 def processDay(userAge):
     day = userAge * 365
-    print("Your age is days is " + str(day))
+    dayResult(day)
     
     return day
 
 def processHour(userAge):
     hour = userAge * 24 * 365
-    print(" You Age in hours is " + str(hour))
+    hoursResult(hour)
     
     return hour
 
 def processMonth(userAge):
     month = userAge * 12
-    print(" your age in months is" + str(month))
+    monthResult(month)
     
     return month
 
 def processSecond(userAge):
     second = userAge * 365 * 24 * 60 * 60
-    print("Your age in seconds is  " + str(second))
+    secondResult(second)
     
     return second
+
+def secondResult(second):
+    print("Your Age in Seconds is:" + str(second))
 
 # Main
 # Create a program that asks the user how old they are in years. Then ask the user if they would like to know how old they are in (M)onths, (D)ays, (H)ours, or (S)econds. Use if/else conditional statements to display their approximate age in the selected timeframe. Do not perform any unnecessary calculations.
