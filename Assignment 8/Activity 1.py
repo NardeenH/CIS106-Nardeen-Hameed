ab98a2a8f19b24fec2bb4d6cb82f3e11eb5eb11a
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-25 13:24:21 -0600

    Add files via upload

--- Assignment 8/Activity 1.py
+++ Assignment 8/Activity 1.py
@@ -1,16 +1,11 @@
-def doLoop(value, exp):
-    count = 1
-    print("Do Loop Counting:")
-    while True:    #This simulates a Do Loop
-        print(str(value) + "*" + str(count) + " = " + str(value * count))
-        count = count + 1
-        if not(count <= exp): break   #Exit loop
+def displayForloop(value, exp, count):
+    print(str(value) + " * " + str(count) + " = " + str(value * count))
 
 def forLoop(value, exp):
     count = 1
     print("For Loop Counting")
     for count in range(1, exp + 1, 1):
-        print(str(value) + " * " + str(count) + " = " + str(value * count))
+        displayForloop(value, exp, count)
 
 def getExp():
     print("Enter the Number of Expressions:")
@@ -23,12 +18,6 @@ def getValue():
     value = int(input())
     
     return value
-
-def whileLoop(value, exp):
-    count = 1
-    print("While Loop Counting:")
-    while count <= exp:
-        count = count + 1
 
 # Main
 # Create a program that uses a loop to generate a list of multiplication expressions for a given value. Ask the user to enter the value and the number of expressions to be displayed.
