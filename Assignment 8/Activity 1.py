024c6e5743ffb8841dc2ff60cfe84609f5f983de
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-25 13:46:48 -0600

    Add files via upload

--- Assignment 8/Activity 1.py
+++ Assignment 8/Activity 1.py
@@ -1,11 +1,15 @@
 def displayForloop(value, exp, count):
     print(str(value) + " * " + str(count) + " = " + str(value * count))
 
-def forLoop(value, exp):
-    count = 1
+def forLoop(value, exp, count):
     print("For Loop Counting")
     for count in range(1, exp + 1, 1):
         displayForloop(value, exp, count)
+
+def getCount():
+    count = 1
+    
+    return count
 
 def getExp():
     print("Enter the Number of Expressions:")
@@ -23,4 +27,5 @@ def getValue():
 # Create a program that uses a loop to generate a list of multiplication expressions for a given value. Ask the user to enter the value and the number of expressions to be displayed.
 value = getValue()
 exp = getExp()
-forLoop(value, exp)
+count = getCount()
+forLoop(value, exp, count)
