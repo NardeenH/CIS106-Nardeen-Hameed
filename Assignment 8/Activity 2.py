6387ca60f2798b0d27cd71e383f1e80e7a4338cc
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-27 14:45:46 -0600

    Add files via upload

--- Assignment 8/Activity 2.py
+++ Assignment 8/Activity 2.py
@@ -1,33 +1,29 @@
 #  Calculate and display the average for the entered scores. 
     
+
 def get_Grade():
-    print("How many grade you woukd like to enter?")
+    print("How many grade you woukd like to enter?");
     numgrade = int(input())
     return numgrade
 
 
-def get_RealGrade(x):
-    print("Enter grade" + str(x + 1) + ":")
-    grade = float(input())
-    return grade
+def get_RealScore(x):
+    print("Enter score" + str(x + 1) + ":");
+    score = float(input())
+    return score
 
 
 def For_Loop(numgrade):
-    total=0
+    total = 0
     for x in range(0, numgrade - 1 + 1, 1):
-        grade = get_RealGrade(x)
-        total= total+grade
+        score = get_RealScore(x)
+        total= total + score
     return total/numgrade
     
-    
-    
+     
 def Main():
     numgrade=get_Grade()    
-    ave = For_Loop(numgrade)
-    print("The average is:" + str(ave))
+    avg = For_Loop(numgrade)
+    print("The average is:" + str(avg));
 
 Main()
-
-
-
-
