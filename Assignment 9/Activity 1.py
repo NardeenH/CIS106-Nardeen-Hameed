871d04087ba65d4b0012a1ecef27020b7abd7ae7
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-02-28 09:42:21 -0600

    Add files via upload

--- Assignment 9/Activity 1.py
+++ Assignment 9/Activity 1.py
@@ -13,8 +13,11 @@ def getValue():
 def processExpression(value, expressions):
     count = 1
     while count <= expressions:
-        print(str(value) + " * " + str(count) + " = " + str(value * count))
+        processLoop(value, expressions, count)
         count = count + 1
+
+def processLoop(value, expressions, count):
+    print(str(value) + " * " + str(count) + " = " + str(value * count))
 
 # Main
 # Create a program that uses a loop to generate a list of multiplication expressions for a given value. Ask the user to enter the value and the number of expressions to be displayed.
