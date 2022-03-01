a8b56eb57b1a588318751a950d2eaf2dec499b63
Dave Braunschweig <dbraunsc@harpercollege.edu>
2022-03-01 17:07:30 -0600

    Assignment 9 corrections

--- Assignment 9/Activity 2.py
+++ Assignment 9/Activity 2.py
@@ -1,38 +1,37 @@
-# Find the average of scores:
+#  Calculate and display the average for the entered scores. 
+    
+
+def get_grade():
+    print("How many grades you woukd like to enter?")
+    numgrade = int(input())
+    return numgrade
 
 
-def get_number():
-    print("Enter Number of scores")
-    number = int(input())        
-    return number
-
-
-def get_score(count):
-    print("score" + str(count + 1) + ":")
+def get_realscore(count):
+    print("Score" + str(count + 1) + ":")
     score = float(input())
     return score
 
 
-def while_loop(number_of_grade):
+def process_grades(number_of_grades):
     total = 0
     count = 0
-    while count < number_of_grade:
-        print("Score" + str(count + 1) + ":")
-        score = float(input())
+    while count < number_of_grades:
+        score = get_realscore(count)
         total = total + score
         count = count + 1
-    average = total / number_of_grade
-    average_result(average)
-    return total / number_of_grade
-    
- 
-def average_result(average):
-    print("The average of the score is:" + str(average))
+    average = total/number_of_grades
+    return total / number_of_grades
+
+
+def display_result(average):
+    print("The average is:" + str(average))
+
+     
+def main():
+    number_of_grades = get_grade()    
+    average = process_grades(number_of_grades)
+    display_result(average)
     
 
-def main():
-    number = get_number()
-    while_loop(number)
-    
-    
 main()
