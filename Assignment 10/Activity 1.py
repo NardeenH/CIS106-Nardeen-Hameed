1fbbf1f0ab0a6e7071f2a5296faf12694d84be9a
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-03-18 09:17:54 -0500

    Add files via upload

--- Assignment 10/Activity 1.py
+++ Assignment 10/Activity 1.py
@@ -5,8 +5,7 @@ def displayResult(average):
 
 def doLoop(total, count):
     while True:    #This simulates a Do Loop
-        print(" Enter your Grade, if you want to stop enter negative value.")
-        grade = int(input())
+        grade = getGrade()
         total = total + grade
         count = count + 1
         if not(0 <= grade): break   #Exit loop
@@ -15,6 +14,12 @@ def doLoop(total, count):
     displayResult(average)
     
     return total
+
+def getGrade():
+    print(" Enter your Grade, if you want to stop enter negative value.")
+    grade = int(input())
+    
+    return grade
 
 # Main
 # Review MathsIsFun: Definition of Average. Create a program that asks the user to enter grade scores. Use a loop to request each score and add it to a total. Continue accepting scores until the user enters either a negative value or no value (your choice). Finally, calculate and display the average for the entered scores.
