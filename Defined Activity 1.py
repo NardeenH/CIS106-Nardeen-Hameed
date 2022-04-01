5f7fd703581f6df65d1e6da4984fe2bb563152b1
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-04-01 11:11:15 -0500

    Add files via upload

---
+++ Defined Activity 1.py
@@ -0,0 +1,42 @@
+# difined Activity
+
+
+def get_year():
+    print("Enter the year")
+    year = int(input())
+    return year
+
+
+def get_month():
+    print(" Enter the month number")
+    month = int(input())
+    return month
+
+
+def leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or ( year % 400 == 0):
+        print("The year", year, " is a leap year")
+        return True
+    else: 
+        print("the year", year, "is not a leap year")
+        return False
+            
+            
+def month_days(year, month):
+    days = [0, 31, 28, 31, 30,31, 30, 31, 30, 31, 30, 31]
+    months = ['','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Novemver', 'December']
+    while month < 13 and month > 0:
+      print(months[month], "has", days[month],"days") 
+      month = int(input())
+      return month
+        
+
+def main():
+    year = get_year()
+    month = get_month()
+    leap_year(year)
+    month_days(year, month)
+    
+    
+main()
+           
