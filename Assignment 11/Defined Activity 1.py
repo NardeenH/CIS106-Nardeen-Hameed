700c9b541f80471976384402cc6dc9bb518c3ac4
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-04-06 18:15:57 -0500

    Add files via upload

--- Assignment 11/Defined Activity 1.py
+++ Assignment 11/Defined Activity 1.py
@@ -23,30 +23,26 @@ def leap_year(year):
             
             
 def display_result(month_name, month_days):
-#     while year > 0 and  month < 13 and month > 0:
         print(month_name, "has", month_days, "days:")
-#         month = int(input("\nEnter a month: "))
-#     return month
+
 
 def get_month_days(month, year):
     days = [0, 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31]
     leap = leap_year(year)
-#         print(months[month], "has", days[month], "days:")
     if leap == True:
         days[2] = 29
-#         print(months[month], "has", days[month], "days:")
     else:
         days[2] = 28
     month_days = days[month]
     return month_days
-#         print(months[month], "has", days[month], "days:")
+
 
 def get_month_name(month):
-     months = ['', 'January', 'February', 'March', 'April', 'May',
-              'June', 'July', 'August', 'September',
-              'October', 'November', 'December']
-     month_name = months[month]
-     return month_name
+    months = ['', 'January', 'February', 'March', 'April', 'May',
+                  'June', 'July', 'August', 'September',
+                  'October', 'November', 'December']
+    month_name = months[month]
+    return month_name
 
             
 def main():
