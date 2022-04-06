69b65e642d4f5d154940f58a378051ac620493d3
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-04-05 21:19:31 -0500

    Add files via upload

--- Assignment 11/Defined Activity 1.py
+++ Assignment 11/Defined Activity 1.py
@@ -16,9 +16,8 @@ def get_month():
 def leap_year(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         print("The year", year, " is a leap year")
-        days[2] = 29
         return True
-    else: 
+    else:
         print("The year", year, "is not a leap year")
         return False
             
