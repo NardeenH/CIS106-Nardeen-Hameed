746abed4de315c9454724e5f7c1841dbb51d770d
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-04-15 17:56:25 -0500

    Update Activity 3.py

--- Assignment 13/Activity 3.py
+++ Assignment 13/Activity 3.py
@@ -1,11 +1,16 @@
 # Assainment 13, Actvity 3
-# # Create a program that asks the user for a line of text. Use string functions/methods to delete leading, trailing, and duplicate spaces, and then print the line of text backwards. For example:
+# # Create a program that asks the user for a line of text.
+# Use string functions/methods to delete leading, trailing, and duplicate spaces,
+# and then print the line of text backwards. For example:
 #       the   cat   in   the   hat  
 #     tah eht ni tac eht
-# Use separate subroutines/functions/methods to implement input, each type of processing, and output. Avoid global variables by passing parameters and returning results.
+# Use separate subroutines/functions/methods to implement input, each type of processing,
+# and output. Avoid global variables by passing parameters and returning results.
+# https://www.datasciencemadesimple.com/remove-spaces-in-python/
+
 
 def get_text():
-    print("Enter your text:")
+    print("Enter your text:\n")
     text = str(input())
     return text
 
