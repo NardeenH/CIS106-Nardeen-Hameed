9580902909c0ad154e8dbb51b1211100bb3b0c1a
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-05-05 13:36:13 -0500

    Update Final Project.xml.py

--- Final Project/Final Project.xml.py
+++ Final Project/Final Project.xml.py
@@ -1,16 +1,28 @@
-#
+# Final
 
 import urllib.request
-import xml.etree.ElemenTree
+import xml.etree.ElementTree as ET
 
-def read_file(filename):
-    try:
-        page = urllib.request.urlopen(filename).read().decode()
-    except Exception as exception:
-        print(str(exception) + " reading " + filename)
-        exit(1)
-for line in page.split("\n"):
-    print(line)
+
+def read_url(filename):   
+    xml_page = ET.parse(filename)
+    return xml_page
+
+
+def process_item(xml_page): 
+    items = []
+    for item in xml_page.iter('CD'):
+        items.append({"title" : item.findtext('TITLE')})
+        print('title')
+    return items
+#def read_file(filename):
+  #  try:
+   #     page = urllib.request.urlopen(filename).read().decode()
+    #except Exception as exception:
+     #   print(str(exception) + " reading " + filename)
+      #  exit(1)
+#for line in page.split("\n"):
+ #   print(line)
     
 
 # url = " https://www.w3schools.com/xml/cd_catalog.xml"
@@ -46,10 +58,11 @@ for line in page.split("\n"):
 #     
 #     
 #     
-def main():
-    
+def main(): 
 #     filename = "https://www.w3schools.com/xml/cd_catalog.xml"
     filename = "cd_catalog.xml"
+    xml = read_url(filename)
+    items = process_item(xml_page)
 
 
 main()
