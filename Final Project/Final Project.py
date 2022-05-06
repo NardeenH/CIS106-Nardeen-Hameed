d17f83a9b17ead1b6908f17b689fd100868e2830
NardeenH <97866278+NardeenH@users.noreply.github.com>
2022-05-06 11:59:01 -0500

    Add files via upload

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -1,8 +1,8 @@
 # https://www.youtube.com/watch?v=rFxXDO8-keg
 # Final Project:
-# Some help from Tim for line 30.
+# Some help from Tim.
 
-import urllib.request
+# import urllib.request
 import xml.etree.ElementTree as ET
 import os
 
@@ -17,21 +17,25 @@ def process_item(xml):
     for item in xml.iter('CD'):
         items.append({
             "title" : item.find('TITLE').text,
-            "artist": item.find('ARTIST').text,
-            "country": item.find('COUNTRY').text,
-            "price": item.find('PRICE').text,
-            "year": item.find('YEAR').text
-            })
+            "artist" : item.find('ARTIST').text,
+            "country" : item.find('COUNTRY').text,
+            "price" : item.find('PRICE').text,
+            "year" : item.find('YEAR').text }) 
     return items
 
 
 def calcalate_average(items):
     total = 0
     count = 0
-    print("Title" + " - " + 'Artist' + " - " + 'Country' + " - " + 'Price' + " - " + 'Year')
+    print("Title" + " - " + 'Artist' +
+          " - " + 'Country' + " - " +
+          'Price' + " - " + 'Year')
     for element in items:
-        print(element['title'] + " - " + element['artist'] + " - " +
-              element['country'] + " - " + element['price'] + " - " + element['year'])
+        print(element['title'] + " - " +
+              element['artist'] + " - " +
+              element['country'] + " - " +
+              element['price'] + " - " +
+              element['year'])
         total = total + float(element['price'])
         count = count + 1
     return (count, total /count)
@@ -43,8 +47,9 @@ def main():
         try:
             xml = read_url(filename)
             items = process_item(xml)
-            count,avr = calcalate_average(items)
-            print(("%d items, $%.2f average price.")%(count,avr))
+            count, average = calcalate_average(items)
+            print((" %d items, $%.2f average price.") % (count,average))
+            print(" Prof Dave, You are the best in the west <3")
         except Exception as exception:
             print(exception)
     else:
